841f96040fa954bc900eb3adb7251a75d0197c89
David Barnes <david.o.barnes@timkensteel.com>
2021-11-22 16:34:47 -0500

    Add project files.

---
+++ ServiceMonitor/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ServiceMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvcolDT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvcolDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartMonitor_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StopMonitor_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Monitor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "minutes";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "minutes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(195, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Auto Restart";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "60";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Interval:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.AutoRestartStop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.AutoRestartStart_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvcolDT,
+            this.lvcolDescription});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 118);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(711, 338);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // lvcolDT
+            // 
+            this.lvcolDT.Text = "Time Stamp";
+            this.lvcolDT.Width = 108;
+            // 
+            // lvcolDescription
+            // 
+            this.lvcolDescription.Text = "Data";
+            this.lvcolDescription.Width = 304;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 468);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Service Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader lvcolDT;
+        private System.Windows.Forms.ColumnHeader lvcolDescription;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ServiceMonitor/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace ServiceMonitor
+{
+    public partial class Form1 : Form
+    {
+        List<string> services = new List<string>();
+
+        System.Windows.Forms.Timer monitorTimer = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer autoRestartTimer = new System.Windows.Forms.Timer();
+
+        ServiceController service = null;
+
+        int AutoRestartTime = 60;
+        int MonitorTime = 30;
+
+        bool AutoRestartEnabled = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            services.Add("TS HSP Ladle Aisle BH data logger");
+            services.Add("TS HSP Ladle Aisle BH 4 tag service");
+            services.Add("TS HSP Ladle Aisle BH 5 tag service");
+            services.Add("TS HSP Ladle Aisle BH 4 memshare producer");
+            services.Add("TS HSP Ladle Aisle BH 5 memshare producer");
+
+            monitorTimer.Tick += new System.EventHandler(monitorTimer_Tick);
+            monitorTimer.Interval = 1800000;
+
+            autoRestartTimer.Tick += new System.EventHandler(autoRestartTimer_Tick);
+            autoRestartTimer.Interval = 3600000;
+        }
+
+        public void Log(string message)
+        {
+            // List View
+            ListViewItem tmp = new ListViewItem(DateTime.Now.ToString());
+            tmp.SubItems.Add(message);
+            listView1.Invoke((MethodInvoker)delegate ()
+            {
+                listView1.Items.Insert(0, tmp);
+                if (listView1.Items.Count > 50)
+                    listView1.Items.RemoveAt(50);
+            });
+        }
+
+
+        public void MonitorService()
+        {
+            foreach (var item in services)
+            {
+                service = new ServiceController(item);
+                try
+                {
+                    if (!service.Status.Equals(ServiceControllerStatus.Running) || AutoRestartEnabled == true)
+                    {
+                        Log($"{item} is {service.Status} & is being restarted.");
+                        RestartService(item, 10000);
+                    }
+                    else
+                    {
+                        Log($"{item} is {service.Status}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log($"error: {ex}");
+                }
+
+            }
+        }
+
+        public void RestartService(string serviceName, int timeoutMilliseconds)
+        {
+            try
+            {
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+                int millisec1 = Environment.TickCount;
+                if (service.Status.Equals(ServiceControllerStatus.Running) || (service.Status.Equals(ServiceControllerStatus.StartPending)))
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                    int millisec2 = Environment.TickCount;
+                    timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                }
+                else if (service.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+                    int millisec2 = Environment.TickCount;
+                    timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                }
+                Log("Service has been reset and the status is " + service.Status);
+            }
+            catch (Exception ex)
+            {
+                Log($"error: {ex}");
+            }
+
+        }
+
+        //********************************************************************************
+        // start monitor
+        //
+        private void StartMonitor_Click(object sender, EventArgs e)
+        {
+            Log($"Service Monitor started.");
+            MonitorTime = Convert.ToInt32(textBox2.Text);
+            monitorTimer.Interval = (MonitorTime * 60000);
+            MonitorService();
+            monitorTimer.Start();
+        }
+        private void StopMonitor_Click(object sender, EventArgs e)
+        {
+            monitorTimer.Stop();
+            Log($"Service Monitor stopped.");
+        }
+        private void monitorTimer_Tick(object sender, EventArgs e)
+        {
+            monitorTimer.Stop();
+            AutoRestartEnabled = false;
+            MonitorService();
+            monitorTimer.Start();
+        }
+        //********************************************************************************
+
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // start auto restart
+        //
+        private void AutoRestartStart_Click(object sender, EventArgs e)
+        {
+            Log("AutoRestart active.");
+            AutoRestartTime = Convert.ToInt32(textBox3.Text);
+            autoRestartTimer.Interval = (AutoRestartTime * 60000);
+            AutoRestartEnabled = true;
+            MonitorService();
+            AutoRestartEnabled = false;
+            autoRestartTimer.Start();
+        }
+
+        private void AutoRestartStop_Click(object sender, EventArgs e)
+        {
+            Log("AutoRestart deactivated.");
+            autoRestartTimer.Stop();
+        }
+
+        private void autoRestartTimer_Tick(object sender, EventArgs e)
+        {
+            autoRestartTimer.Stop();
+            AutoRestartEnabled = true;
+            MonitorService();
+            AutoRestartEnabled = false;
+            autoRestartTimer.Start();
+        }
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
